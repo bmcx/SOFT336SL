4e201cab83542c7d15a9728f9b4ec9dfa12bfd50
Chandima Bandara <bmcbandara@hotmail.com>
2021-11-02 16:47:12 +0530

    add: mac ss
    fix: minor sizing issue on mac design

--- TempConverter/widget.cpp
+++ TempConverter/widget.cpp
@@ -9,6 +9,7 @@ Widget::Widget(QWidget *parent)
     tc = new TempConverter(this);
     QObject::connect(ui->dialCelsius, SIGNAL(valueChanged(int)), tc, SLOT(setTempCelsius(int)));
     QObject::connect(ui->dialFahrenheit, SIGNAL(valueChanged(int)), tc, SLOT(setTempFahrenheit(int)));
+
     QObject::connect(tc, SIGNAL(tempCelsiusChanged(int)), ui->dialCelsius, SLOT(setValue(int)));
     QObject::connect(tc, SIGNAL(tempFahrenheitChanged(int)), ui->dialFahrenheit, SLOT(setValue(int)));
 }
